deed695e96d96a4db5fbd766ff3b45589471c0b1
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-08 15:38:07 -0700

    [feat] react App mounts to page
    -lays out basic react components structure.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // state = {
+
+    // };
+  }
+
+  render() {
+    return (
+      <div>React is Mounted</div>
+    );
+  }
+}
+
+
+export default App;
