5c0723df9a0c122eb5b7b6a35206b6c3d511b460
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-14 13:13:36 -0700

    [prog] review populates with fit infor [style] added classnames for review box [style] added summary style 'bars'

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 
 const Review = ({ review }) =>
-  <section className='review'>
-    <section className='reviewHead'>
-      <section className='reviewRating'>Stars: {review.rating}</section>
-      <section className='reviewDate'>{review.date}</section>
-      <section className='reviewTitle'>{review.reviewTitle}</section>
+  <section className="review">
+    <section className="reviewHead">
+      <section className="reviewRating">Stars: {review.rating} image here</section>
+      <section className="reviewDate">{review.date}</section>
     </section>
-    <section className='reviewBody'>{review.reviewBody}</section>
-    <section className='userBox'>
-      <section className='userCard' className='userAvatar'>
-        <img src={review.userAvatar} alt='user avatar'></img>
+    <section className="reviewTitle">{review.reviewTitle}</section>
+    {review.fit && <div className="reviewHasFit">
+      <section className="reviewFamiliarity">Familiarity: {review.familiarity}</section>
+      <section className="reviewFit">Fit: {review.fit}</section>
+      <section className="reviewSize">Size Bought: {review.size}</section>
+      <section className="reviewHeight">Height: {review.userHeight}</section>
+      <section className="reviewWeight">Weight: {review.userWeight}lbs</section>
+    </div>
+    }
+    <section className="reviewBody">{review.reviewBody}</section>
+    <section className="userBox">
+      <section className="userCard" className="userAvatar">
+        <img src={review.userAvatar} alt="user avatar"></img>
       </section>
-      <section className='userCard' className='username'>{review.username}</section>
+      <section className="userCard" className="username">{review.username}</section>
     </section>
-    <section className='commentBox'>
-      <section className='commentArea'>Leave a Comment</section>
-      <section className='commentArea'>Flag Inapproprite</section>
-      <section className='commentArea'>Like a Comment</section>
+    <section className="commentBox">
+      <section className="commentArea">Leave a Comment</section>
+      <section className="commentArea">Flag Inapproprite</section>
+      <section className="commentArea">Like a Comment</section>
     </section>
     <br></br>
-    {/* <div>{review.username} {review.userHeight && `is ${review.userHeight}'' and ${review.userWeight}lbs.`}</div> */}
+    {/* <div>{review.username} {review.userHeight && `is ${review.userHeight}"" and ${review.userWeight}lbs.`}</div> */}
   </section>;
 
 export default Review;
--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -10,23 +10,66 @@ class Summary extends React.Component {
   render() {
     if (this.props.reviews && this.props.summaryData) {
       return (
-        <div className='summaryBlock'>
+        <div className='summary-container'>
           <div className='avgRating'>
-            Overall Rating
-            <div>{this.props.summaryData.avgRating.toFixed(1)} based on {this.props.reviews.length} ratings</div>
+            <div className='avgRating-title'>Overall Rating</div>
+            <div className="avgRating-subtitle">{this.props.summaryData.avgRating.toFixed(1)} based on {this.props.reviews.length} ratings</div>
+            <div>starsimagehere</div>
           </div>
           <div className='starsBar'>
-            Review Summary
-            <ul>
-              <li>No. of 5 Star Ratings: {this.props.summaryData.ratingsObj[5]} / barChart: {this.props.summaryData.ratingsObj.star5Bar}%</li>
-              <li>No. of 4 Star Ratings: {this.props.summaryData.ratingsObj[4]} / barChart: {this.props.summaryData.ratingsObj.star4Bar}%</li>
-              <li>No. of 3 Star Ratings: {this.props.summaryData.ratingsObj[3]} / barChart: {this.props.summaryData.ratingsObj.star3Bar}%</li>
-              <li>No. of 2 Star Ratings: {this.props.summaryData.ratingsObj[2]} / barChart: {this.props.summaryData.ratingsObj.star2Bar}%</li>
-              <li>No. of 1 Star Ratings: {this.props.summaryData.ratingsObj[1]} / barChart: {this.props.summaryData.ratingsObj.star1Bar}%</li>
-            </ul>
+            <div className="starsBar-title">Review Summary</div>
+            <figure>
+              <a className="starsBar-stars" data-rank="5">5 Stars</a>
+              <div className="starsBar-bar bar5">
+                <span></span>
+                <div>{this.props.summaryData.ratingsObj.star5Bar}%</div>
+              </div>
+              <span className="starsBar-reviews-number">
+                <a className="five-star-bar" data-rank="5" data-quantity={this.props.summaryData.ratingsObj[5]}>{this.props.summaryData.ratingsObj[5]}</a>
+              </span>
+
+              <a className="starsBar-stars" data-rank="4">4 Stars</a>
+              <div className="starsBar-bar bar4">
+                <span></span>
+                <div>{this.props.summaryData.ratingsObj.star4Bar}%</div>
+              </div>
+              <span className="starsBar-reviews-number">
+                <a className="five-star-bar" data-rank="4" data-quantity={this.props.summaryData.ratingsObj[4]}>{this.props.summaryData.ratingsObj[4]}</a>
+              </span>
+
+              <a className="starsBar-stars" data-rank="3">3 Stars</a>
+              <div className="starsBar-bar bar3">
+                <span></span>
+                <div>{this.props.summaryData.ratingsObj.star3Bar}%</div>
+              </div>
+              <span className="starsBar-reviews-number">
+                <a className="five-star-bar" data-rank="3" data-quantity={this.props.summaryData.ratingsObj[3]}>{this.props.summaryData.ratingsObj[3]}</a>
+              </span>
+
+              <a className="starsBar-stars" data-rank="2">2 Stars</a>
+              <div className="starsBar-bar bar2">
+                <span></span>
+                <div>{this.props.summaryData.ratingsObj.star2Bar}%</div>
+              </div>
+              <span className="starsBar-reviews-number">
+                <a className="five-star-bar" data-rank="2" data-quantity={this.props.summaryData.ratingsObj[2]}>{this.props.summaryData.ratingsObj[2]}</a>
+              </span>
+
+              <a className="starsBar-stars" data-rank="1">1 Stars</a>
+              <div className="starsBar-bar bar1">
+                <span></span>
+                <div>{this.props.summaryData.ratingsObj.star1Bar}%</div>
+              </div>
+              <span className="starsBar-reviews-number">
+                <a className="five-star-bar" data-rank="1" data-quantity={this.props.summaryData.ratingsObj[1]}>{this.props.summaryData.ratingsObj[1]}</a>
+              </span>
+
+
+            </figure>
           </div>
+
           <div className='fitBar'>
-            Avg Fit Goes Here
+            <div className="fitBar-title">Avg Fit Goes Here</div>
             <ul>
               <li>Avg Fit: {this.props.summaryData.avgFit.toFixed(2)}%</li>
               <li>Runs Small: {this.props.summaryData.fitsObj['Runs Small']}</li>
