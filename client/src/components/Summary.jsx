3f70232a56a691b104cadd8a5d8ef1f743cb830a
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-16 17:22:17 -0700

    [feat] summary shows 0 on right
    instead of null

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -29,89 +29,38 @@ class Summary extends React.Component {
               <div className="starsBar-stars">5 Stars</div>
               <div className="summary-bar bar5"></div>
               <div className="starsBar-num">
-                {this.props.summaryData.ratingsObj[5]}
+                {this.props.summaryData.ratingsObj[5] || 0}
               </div>
             </div>
             <div>
               <div className="starsBar-stars">4 Stars</div>
               <div className="summary-bar bar4"></div>
               <div className="starsBar-num">
-                {this.props.summaryData.ratingsObj[4]}
+                {this.props.summaryData.ratingsObj[4] || 0}
               </div>
             </div>
             <div>
               <div className="starsBar-stars">3 Stars</div>
               <div className="summary-bar bar3"></div>
               <div className="starsBar-num">
-                {this.props.summaryData.ratingsObj[3]}
+                {this.props.summaryData.ratingsObj[3] || 0}
               </div>
             </div>
             <div>
               <div className="starsBar-stars">2 Stars</div>
               <div className="summary-bar bar2"></div>
               <div className="starsBar-num">
-                {this.props.summaryData.ratingsObj[2]}
+                {this.props.summaryData.ratingsObj[2] || 0}
               </div>
             </div>
             <div>
               <div className="starsBar-stars">1 Stars</div>
               <div className="summary-bar bar1"></div>
               <div className="starsBar-num">
-                {this.props.summaryData.ratingsObj[1]}
+                {this.props.summaryData.ratingsObj[1] || 0}
               </div>
             </div>
           </div>
-
-          {/* <div className='starsBar'>
-            <div className="starsBar-title">Review Summary</div>
-            <figure>
-              <a className="starsBar-stars" data-rank="5">5 Stars</a>
-              <div className="starsBar-bar bar5">
-                <span></span>
-                <div>{this.props.summaryData.ratingsObj.star5Bar.toFixed(2)}%</div>
-              </div>
-              <span className="starsBar-reviews-number">
-                <a className="five-star-bar" data-rank="5" data-quantity={this.props.summaryData.ratingsObj[5]}>{this.props.summaryData.ratingsObj[5]}</a>
-              </span>
-
-              <a className="starsBar-stars" data-rank="4">4 Stars</a>
-              <div className="starsBar-bar bar4">
-                <span></span>
-                <div>{this.props.summaryData.ratingsObj.star4Bar.toFixed(2)}%</div>
-              </div>
-              <span className="starsBar-reviews-number">
-                <a className="five-star-bar" data-rank="4" data-quantity={this.props.summaryData.ratingsObj[4]}>{this.props.summaryData.ratingsObj[4]}</a>
-              </span>
-
-              <a className="starsBar-stars" data-rank="3">3 Stars</a>
-              <div className="starsBar-bar bar3">
-                <span></span>
-                <div>{this.props.summaryData.ratingsObj.star3Bar.toFixed(2)}%</div>
-              </div>
-              <span className="starsBar-reviews-number">
-                <a className="five-star-bar" data-rank="3" data-quantity={this.props.summaryData.ratingsObj[3]}>{this.props.summaryData.ratingsObj[3]}</a>
-              </span>
-
-              <a className="starsBar-stars" data-rank="2">2 Stars</a>
-              <div className="starsBar-bar bar2">
-                <span></span>
-                <div>{this.props.summaryData.ratingsObj.star2Bar.toFixed(2)}%</div>
-              </div>
-              <span className="starsBar-reviews-number">
-                <a className="five-star-bar" data-rank="2" data-quantity={this.props.summaryData.ratingsObj[2]}>{this.props.summaryData.ratingsObj[2]}</a>
-              </span>
-
-              <a className="starsBar-stars" data-rank="1">1 Stars</a>
-              <div className="starsBar-bar bar1">
-                <span></span>
-                <div>{this.props.summaryData.ratingsObj.star1Bar.toFixed(2)}%</div>
-              </div>
-              <span className="starsBar-reviews-number">
-                <a className="five-star-bar" data-rank="1" data-quantity={this.props.summaryData.ratingsObj[1]}>{this.props.summaryData.ratingsObj[1]}</a>
-              </span>
-
-            </figure>
-          </div> */}
 
           <div className='fitBar'>
             <div className="fitBar-title">Fits True To Size</div>
