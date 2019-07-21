1b7e67b938ac177d99e2107ec8150a4fe8762709
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-21 12:00:34 -0700

    [visual] reduces space between summary bar rows

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -25,35 +25,35 @@ class Summary extends React.Component {
 
           <div className='starsBar'>
             <div className="starsBar-title">Review Summary</div><br></br>
-            <div className="col-3">
+            <div className="star-row">
               <div className="starsBar-stars">5 Stars</div>
               <div className="summary-bar bar5"></div>
               <div className="starsBar-num">
                 {this.props.summaryData.ratingsObj[5] || 0}
               </div>
             </div>
-            <div className="col-3">
+            <div className="star-row">
               <div className="starsBar-stars">4 Stars</div>
               <div className="summary-bar bar4"></div>
               <div className="starsBar-num">
                 {this.props.summaryData.ratingsObj[4] || 0}
               </div>
             </div>
-            <div className="col-3">
+            <div className="star-row">
               <div className="starsBar-stars">3 Stars</div>
               <div className="summary-bar bar3"></div>
               <div className="starsBar-num">
                 {this.props.summaryData.ratingsObj[3] || 0}
               </div>
             </div>
-            <div className="col-3">
+            <div className="star-row">
               <div className="starsBar-stars">2 Stars</div>
               <div className="summary-bar bar2"></div>
               <div className="starsBar-num">
                 {this.props.summaryData.ratingsObj[2] || 0}
               </div>
             </div>
-            <div className="col-3">
+            <div className="star-row">
               <div className="starsBar-stars">1 Stars</div>
               <div className="summary-bar bar1"></div>
               <div className="starsBar-num">
